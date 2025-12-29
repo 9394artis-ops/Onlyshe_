bd0a181310da319ffdd9d9dcc736db8140aa0b58
9394artis-ops <9394artis@gmail.com>
2025-12-29 21:46:31 +0530

    Remove clipboard copy functionality from script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-const btn = document.querySelector('.copy-btn');
-
-btn.addEventListener('click', () => {
-  const text = document.querySelector('.quote-hindi').innerText;
-  navigator.clipboard.writeText(text);
-  btn.innerText = "Copied ❤️";
-  setTimeout(()=>btn.innerText="Copy",2000);
-});
+console.log("onlyshe website loaded ❤️");
