3954111e103f7fea88652047ec410ace5f38b889
9394artis-ops <9394artis@gmail.com>
2025-12-29 23:06:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("onlyshe website loaded ❤️");
+function copyText(id) {
+  const text = document.getElementById(id).innerText;
+  navigator.clipboard.writeText(text).then(() => {
+    alert("Quote copied!");
+  });
+}
